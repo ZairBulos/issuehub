31491c2db5dfd4f2e0b59d56e7d9ec29c05a4d72
Zair Bulos <buloszair@gmail.com>
2026-02-26 19:59:53 -0300

    test(auth): add unit and integration tests for AuthController request login endpoint

--- src/test/java/com/issuehub/modules/auth/infrastructure/adapters/in/http/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/issuehub/modules/auth/infrastructure/adapters/in/http/controller/AuthControllerIntegrationTest.java
@@ -1,15 +1,16 @@
 package com.issuehub.modules.auth.infrastructure.adapters.in.http.controller;
 
 import com.issuehub.IntegrationTest;
-import com.issuehub.modules.auth.domain.models.valueobjects.VerificationCode;
+import com.issuehub.modules.auth.infrastructure.adapters.in.http.dto.RequestLoginRequest;
 import com.issuehub.modules.auth.infrastructure.adapters.out.persistence.repositories.EmailVerificationJpaRepository;
-import com.issuehub.shared.domain.model.EntityId;
+import com.issuehub.modules.auth.infrastructure.adapters.out.persistence.repositories.LoginVerificationJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.Instant;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -23,14 +24,21 @@ public class AuthControllerIntegrationTest {
     @Autowired
     private EmailVerificationJpaRepository emailVerificationRepository;
 
-    private static final String CLEAN_DB = "/db/clean/email_verifications.sql";
-    private static final String DATA_DB = "/db/data/email_verifications.sql";
+    @Autowired
+    private LoginVerificationJpaRepository loginVerificationRepository;
+
+    private static final String CLEAN_DB_EMAIL_VERIFICATIONS = "/db/clean/email_verifications.sql";
+    private static final String DATA_DB_EMAIL_VERIFICATIONS = "/db/data/email_verifications.sql";
+
+    private static final String CLEAN_DB_LOGIN_VERIFICATIONS = "/db/clean/login_verifications.sql";
+    private static final String DATA_DB_LOGIN_VERIFICATIONS = "/db/data/login_verifications.sql";
 
     private final String VERIFY_EMAIL = AuthController.AUTH + AuthController.VERIFY_EMAIL;
+    private final String REQUEST_LOGIN = AuthController.AUTH + AuthController.REQUEST_LOGIN;
 
     // === verify email ===
     @Test
-    @Sql({CLEAN_DB, DATA_DB})
+    @Sql({CLEAN_DB_EMAIL_VERIFICATIONS, DATA_DB_EMAIL_VERIFICATIONS})
     void verifyEmail_shouldReturn204NoContent_whenVerificationSucceeds() {
         // Given
         var developerId = "11111111-1111-1111-1111-111111111111";
@@ -50,21 +58,39 @@ public class AuthControllerIntegrationTest {
                 .hasValueSatisfying(v -> assertThat(v.getUsedAt()).isNotNull());
     }
 
+    // === request login ===
     @Test
-    @Sql(CLEAN_DB)
-    void verifyEmail_shouldReturn400BadRequest_whenVerificationNotFound() {
+    @Sql({CLEAN_DB_LOGIN_VERIFICATIONS, DATA_DB_LOGIN_VERIFICATIONS})
+    void requestLogin_shouldReturn200Ok_whenLoginRequestSucceeds() {
         // Given
-        var developerId = EntityId.generate().value();
-        var code = VerificationCode.generate().value();
+        var developerEmail = "request_login@example.com";
+        var request = new RequestLoginRequest(developerEmail);
 
         // When
-        var response = restTemplate.getForEntity(
-                VERIFY_EMAIL + "?developerId=" + developerId + "&code=" + code,
+        var response = restTemplate.postForEntity(
+                REQUEST_LOGIN,
+                request,
                 Void.class
         );
 
         // Then
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        var loginVerifications = loginVerificationRepository.findAll();
+        assertThat(loginVerifications.size()).isEqualTo(2);
+
+        var invalidated = loginVerifications.stream()
+                .filter(v -> v.getId().equals(UUID.fromString("00000000-0000-0000-0000-000000000000")))
+                .findFirst()
+                .orElseThrow();
+        assertThat(invalidated.getUsedAt()).isNotNull();
+
+        var validated = loginVerifications.stream()
+                .filter(v -> !v.getId().equals(invalidated.getId()))
+                .findFirst()
+                .orElseThrow();
+        assertThat(validated.getUsedAt()).isNull();
+        assertThat(validated.getExpiresAt()).isAfter(Instant.now());
     }
 
 }
--- src/test/java/com/issuehub/modules/auth/infrastructure/adapters/in/http/controller/AuthControllerTest.java
+++ src/test/java/com/issuehub/modules/auth/infrastructure/adapters/in/http/controller/AuthControllerTest.java
@@ -1,22 +1,30 @@
 package com.issuehub.modules.auth.infrastructure.adapters.in.http.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.issuehub.modules.auth.application.dto.internal.VerifyEmailCommand;
+import com.issuehub.modules.auth.application.exceptions.AccountBlockedException;
+import com.issuehub.modules.auth.application.exceptions.AccountNotFoundException;
+import com.issuehub.modules.auth.application.exceptions.AccountNotVerifiedException;
+import com.issuehub.modules.auth.application.ports.in.internal.RequestLoginUseCase;
 import com.issuehub.modules.auth.application.ports.in.internal.VerifyEmailUseCase;
 import com.issuehub.modules.auth.domain.exceptions.InvalidVerificationCodeException;
 import com.issuehub.modules.auth.domain.exceptions.VerificationCodeAlreadyUsedException;
 import com.issuehub.modules.auth.domain.exceptions.VerificationCodeExpiredException;
 import com.issuehub.modules.auth.domain.models.valueobjects.VerificationCode;
+import com.issuehub.modules.auth.infrastructure.adapters.in.http.dto.RequestLoginRequest;
 import com.issuehub.shared.domain.model.EntityId;
 import com.issuehub.shared.infrastructure.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(SecurityConfig.class)
@@ -26,10 +34,17 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockitoBean
     private VerifyEmailUseCase verifyEmailUseCase;
 
+    @MockitoBean
+    private RequestLoginUseCase requestLoginUseCase;
+
     private final String VERIFY_EMAIL = AuthController.AUTH + AuthController.VERIFY_EMAIL;
+    private final String REQUEST_LOGIN = AuthController.AUTH + AuthController.REQUEST_LOGIN;
 
     // === verify email ===
     @Test
@@ -120,6 +135,96 @@ class AuthControllerTest {
         mockMvc.perform(get(VERIFY_EMAIL)
                         .param("developerId", command.developerId().value().toString())
                         .param("code", command.code().value()))
+                .andExpect(status().isBadRequest());
+    }
+
+    // === request login ===
+    @Test
+    void requestLogin_shouldReturn200Ok_whenLoginRequestSucceeds() throws Exception {
+        // Given
+        var email = "request_login@email.com";
+        var request = new RequestLoginRequest(email);
+
+        doNothing().when(requestLoginUseCase).execute(email);
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+
+        verify(requestLoginUseCase).execute(email);
+    }
+
+    @Test
+    void requestLogin_shouldReturn404NotFound_whenAccountNotFound() throws Exception {
+        // Given
+        var email = "missing@example.com";
+        var request = new RequestLoginRequest(email);
+
+        doThrow(new AccountNotFoundException("Account not found " + email))
+                .when(requestLoginUseCase).execute(email);
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void requestLogin_shouldReturn403Forbidden_whenAccountBlocked() throws Exception {
+        // Given
+        var email = "blocked@example.com";
+        var request = new RequestLoginRequest(email);
+
+        doThrow(new AccountBlockedException("Account is blocked " + email))
+                .when(requestLoginUseCase).execute(email);
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void requestLogin_shouldReturn403Forbidden_whenAccountNotVerified() throws Exception {
+        // Given
+        var email = "not_verified@example.com";
+        var request = new RequestLoginRequest(email);
+
+        doThrow(new AccountNotVerifiedException("Account is not verified " + email))
+                .when(requestLoginUseCase).execute(email);
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void requestLogin_shouldReturn400BadRequest_whenEmailIsBlank() throws Exception {
+        // Given
+        var request = new RequestLoginRequest("");
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void requestLogin_shouldReturn400BadRequest_whenEmailIsInvalid() throws Exception {
+        // Given
+        var request = new RequestLoginRequest("invalid-email");
+
+        // When/Then
+        mockMvc.perform(post(REQUEST_LOGIN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest());
     }
 
