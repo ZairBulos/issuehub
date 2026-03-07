689eb80fb55ffc9064024a77c7db1f2a125ceed8
Zair Bulos <buloszair@gmail.com>
2026-03-07 19:57:53 -0300

    test(developers): add integration test for DeletedDeveloperLoggedInListener

---
+++ src/test/java/com/issuehub/modules/developers/infrastructure/adapters/in/messaging/DeletedDeveloperLoggedInListenerIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.issuehub.modules.developers.infrastructure.adapters.in.messaging;
+
+import com.issuehub.IntegrationTest;
+import com.issuehub.modules.developers.infrastructure.adapters.out.persistence.repositories.DeveloperJpaRepository;
+import com.issuehub.shared.domain.events.DeletedDeveloperLoggedIn;
+import com.issuehub.shared.domain.model.EntityId;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IntegrationTest
+@ApplicationModuleTest
+class DeletedDeveloperLoggedInListenerIntegrationTest {
+
+    @Autowired
+    private DeveloperJpaRepository developerRepository;
+
+    private static final String CLEAN_DB = "/db/clean/developers.sql";
+    private static final String DATA_DB = "/db/data/developers.sql";
+
+    @Test
+    @Sql({CLEAN_DB, DATA_DB})
+    void whenDeletedDeveloperLoggedInEventPublished_thenDeveloperIsReactivated(Scenario scenario) {
+        // Given
+        var developerId = new EntityId(UUID.fromString("bc13cfa1-2a21-401e-abd5-ec324b984f4c"));
+        var event = new DeletedDeveloperLoggedIn(developerId, Instant.now());
+
+        // When/Then
+        scenario.publish(event)
+                .andWaitForEventOfType(DeletedDeveloperLoggedIn.class)
+                .toArriveAndVerify( deletedDeveloperLoggedIn-> {
+                    assertThat(deletedDeveloperLoggedIn.developerId()).isEqualTo(event.developerId());
+                });
+
+        var developer = developerRepository.findById(event.developerId().value()).orElseThrow();
+        assertThat(developer.getStatus()).isEqualTo("active");
+    }
+
+}
